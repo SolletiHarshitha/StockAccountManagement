d52776ffe908f7c516ff82bb9098f2f3ff611456
SolletiHarshitharigin <solletiharshitha@gmail.com>
2021-07-07 17:38:07 +0530

    [Harshitha] Dispalying Stock Report

---
+++ StockAccountManagement/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StockAccountManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stocks stock = new Stocks();
+            stock.Stock_Management();
+            stock.CalculateEachStockValue();
+            stock.CalculateTotalStockValue();
+            Console.Read();
+        }
+    }
+}
