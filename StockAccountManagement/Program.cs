02e80d3e4ba97315f1dc314a078b449b0d9ce972
SolletiHarshitharigin <solletiharshitha@gmail.com>
2021-07-07 23:28:05 +0530

    [Harshitha] Commercial Data Processing

--- StockAccountManagement/Program.cs
+++ StockAccountManagement/Program.cs
@@ -1,16 +1,50 @@
 ﻿using System;
-
+using Newtonsoft.Json;
+using System.IO;
 namespace StockAccountManagement
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Creating object
-            Stocks stock = new Stocks();
-            stock.Stock_Management();
-            stock.CalculateEachStockValue();
-            stock.CalculateTotalStockValue();
+            Console.WriteLine(" Welcome To Stock Amount Management\n");
+            Console.WriteLine(" Customer Records\n");
+            bool alive = true;
+            while (alive)
+            {
+                Customer customerRecords = JsonConvert.DeserializeObject<Customer>(File.ReadAllText(@"D:\tvstraining\StockAccountManagement\StockAccountManagement\CustomerDetails.json"));
+                Stocks stockAccount = JsonConvert.DeserializeObject<Stocks>(File.ReadAllText(@"D:\tvstraining\StockAccountManagement\StockAccountManagement\StockDetails.json"));
+                Console.WriteLine("\n1.Buy Stocks\n2.Sell Stocks\n3.View Stocks\n4.View Customer Records\n5.View Transaction Details\n6.Exit");
+                Console.WriteLine("Enter your option:");
+                int option = Convert.ToInt32(Console.ReadLine());
+                StockManagement stockManagement = new StockManagement();
+                switch (option)
+                {
+                    case 1:
+                        stockManagement.BuyStocks(customerRecords);
+                        break;
+                    case 2:
+                        stockManagement.SellStocks(customerRecords);
+                        break;
+                    case 3:
+                        stockManagement.ViewStocks();
+                        break;
+                    case 4:
+                        stockManagement.ViewCustomerRecords();
+                        break;
+                    case 5:
+                        stockManagement.ViewTransactions();
+                        break;
+                    case 6:
+                        alive = false;
+                        System.Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Output!!");
+                        break;
+
+                }
+            }
             Console.Read();
         }
     }
